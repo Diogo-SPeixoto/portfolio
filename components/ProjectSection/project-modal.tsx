183d3492a058486563806ea5145712ac8d22181e
Diogo-Peixoto <diogosoarespeixoto41@gmail.com>
2025-07-09 19:17:19 -0300

    feat: enhance ProjectModal with platform details and improved layout

--- components/ProjectSection/project-modal.tsx
+++ components/ProjectSection/project-modal.tsx
@@ -5,10 +5,11 @@ import Image2 from "@/assets/loovi-2.png"
 import Image3 from "@/assets/loovi-3.png"
 import Image4 from "@/assets/loovi-4.png"
 import Image5 from "@/assets/loovi-5.png"
+import { Badge } from "../ui/badge"
 
 export const ProjectModal = ()=>{
     return(
-        <DialogContent className="bg-[#252525] text-white border-none pt-10 pb-4 px-5 h-[90vh]">
+        <DialogContent className="bg-[#252525] text-white border-none pt-10 pb-4 px-5 h-[90vh] w-[96%]">
             <div className="
                 px-5
                 flex flex-col
@@ -21,12 +22,20 @@ export const ProjectModal = ()=>{
                 [&::-webkit-scrollbar-thumb]:rounded-full
                 [&::-webkit-scrollbar-thumb]:bg-gray-300
             ">
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center gap-4 flex-col sm:flex-row">
                         <div className="flex flex-col gap-6">
                             <DialogTitle className="text-4xl font-bold">Loovi</DialogTitle>
                             <div className="flex flex-col gap-4">
+                                <p className="text-base font-semibold text-[#8d8d8d]">Plataformas</p>
+                                <ul className="text-base flex gap-3">
+                                    <li>IOS</li>
+                                    <li>Android</li>
+                                    <li>Web</li>
+                                </ul>
+                            </div>
+                            <div className="flex flex-col gap-4">
                                 <p className="text-base font-semibold text-[#8d8d8d]">Serviços prestado</p>
-                                <ul className="text-sm list-disc pl-6 flex flex-col gap-1">
+                                <ul className="text-xs sm:text-sm list-disc pl-6 flex flex-col gap-1">
                                     <li>Criação e implementação de novas funcionalidades</li>
                                     <li>Resolução e identificação de bugs</li>
                                     <li>Gerenciamento de repositórios</li>
@@ -39,13 +48,13 @@ export const ProjectModal = ()=>{
                                 </ul>
                             </div>
                         </div>
-                        <Image className="max-w-md" alt="" src={Image1} />
+                        <Image className="max-w-52 lg:max-w-md" alt="" src={Image1} />
                 </div>
 
-                <div className="flex justify-between items-center">
-                        <Image className="max-w-md" alt="" src={Image2} />
+                <div className="flex justify-between items-center gap-4 flex-col-reverse sm:flex-row">
+                        <Image className="max-w-52 lg:max-w-md" alt="" src={Image2} />
 
-                        <div className="flex flex-col gap-4 max-w-md text-sm">
+                        <div className="flex flex-col gap-4 max-w-md text-xs sm:text-sm">
                             <p className="text-base font-semibold text-[#8d8d8d]">Sobre o projeto</p>
                             <p className="mb-2">
                                 A Loovi buscava uma solução tecnologicamente avançada, com um design moderno e 
@@ -58,23 +67,14 @@ export const ProjectModal = ()=>{
                                 os usuários e um sistema interno eficiente para gestão, garantindo uma 
                                 solução completa e integrada.
                             </p>
-                            <p className="text-base font-semibold text-[#8d8d8d]">Observação</p>
-                            <p>
-                                Atualmente sou um dos desenvolvedores principais do aplicativo mobile , 
-                                atuando em de ponta a ponta.
-                            </p>
-                            <p>
-                                Trabalhando neste projeto pude  me aperfeiçoar no desenvolvimento mobile, 
-                                além de conhecer novas estruturas e arquiteturas.
-                            </p>
 
                         </div>
                 </div>
 
-                <div className="flex items-center justify-around">
-                        <Image alt="" src={Image3} />
-                        <Image alt="" src={Image4} />
-                        <Image alt="" src={Image5} />
+                <div className="flex items-center justify-around flex-wrap md:flex-nowrap gap-6">
+                    <Image className="max-w-48 lg:max-w-72" alt="" src={Image3} />
+                    <Image className="max-w-48 lg:max-w-72" alt="" src={Image4} />
+                    <Image className="max-w-48 lg:max-w-72" alt="" src={Image5} />
                 </div>
             </div>
 
